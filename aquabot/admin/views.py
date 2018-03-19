248353fa1b7d825e85a8032131a51d190133f0d2
Brian Maci <briancmaci@gmail.com>
2018-03-18 23:22:36 -0400

    Registration working :)

--- aquabot/admin/views.py
+++ aquabot/admin/views.py
@@ -1,7 +1,7 @@
-from flask import current_app, render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for
 from . import admin
-from .forms import LoginForm
-from ..models import User
+from .forms import LoginForm, RegistrationForm
+from ..models import db, User
 from flask_login import current_user, login_user, login_required, logout_user
 from werkzeug.urls import url_parse
 
@@ -20,9 +20,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        # Return with registration
-        # if user is None or not user.check_password(form.password.data):
-        if user is None:
+        if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('.login'))
         login_user(user, remember=form.remember_me.data)
@@ -31,3 +29,23 @@ def login():
             next_page = url_for('.index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@admin.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.date, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('.login'))
+    return render_template('register.html', title='Register', form=form)
+
+@admin.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('.index'))
