836e623597ab148cb5d24179999513c220622d06
Brian Maci <briancmaci@gmail.com>
2018-03-18 23:28:09 -0400

    oops form.date to form.data

--- aquabot/admin/views.py
+++ aquabot/admin/views.py
@@ -37,7 +37,7 @@ def register():
         return redirect(url_for('.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.date, email=form.email.data)
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
