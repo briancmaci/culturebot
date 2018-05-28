c954c64a57be5ed49a7b76cd1f3771225492029f
Brian Maci <briancmaci@gmail.com>
2018-05-28 13:04:07 -0400

    send next

--- aquabot/slackbot/routes.py
+++ aquabot/slackbot/routes.py
@@ -37,6 +37,39 @@ def send_fact(fact_id):
     db.session.commit()
     return 'OK'
 
+
+@slackbot.route('/send-next-fact', methods=['GET'])
+def send_next_fact():
+    fact = Post.query.filter_by(shown=False).first()
+
+    if fact is None:
+        errorResponse = Response(
+            response="All facts have been sent! :(",
+            status=404,
+            mimetype='application/json'
+        )
+        return errorResponse
+
+    shown_index = len(Post.query.filter_by(shown=False).all())
+    additional_facts_list = AdditionalFact.query.filter_by(post_id=fact.id).all()
+    tag_buttons_list = TagButton.query.filter_by(post_id=fact.id).all()
+
+    message_text = "Aqua :trans_heart: Aaptiv"
+    message_attachments = [fact.slack_serialize(get_color(shown_index), additional_facts_list, tag_buttons_list)]
+    slack_bot_token = os.environ["SLACK_API_TOKEN"]
+    slack_channel = os.environ["SLACK_CHANNEL"]
+    slack_client = SlackClient(slack_bot_token)
+
+    response = jsonify(message_attachments)
+    response.status_code = 200
+
+    slack_client.api_call("chat.postMessage", channel=slack_channel, text=message_text, attachments=message_attachments)
+
+    fact.shown = True
+    db.session.commit()
+    return 'OK'
+
+
 def get_color(offset):
     colors: List[str] = ['#e70000', '#ff8c00', '#ffef00', '#00811f', '#0044ff', '#760089']
     color_index = int(offset) % len(colors) - 1
